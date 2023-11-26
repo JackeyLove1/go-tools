757fb9a8f565f4a1ad9c61f9fde0296f674a51f4
fanhuanjie <fanhuanjie@bytedance.com>
2023-11-26 15:07:07 +0800

    options

--- practice/v1/v3/main.go
+++ practice/v1/v3/main.go
@@ -1,5 +1,59 @@
 package main
 
-func main() {
+import "fmt"
 
+type User struct {
+    Name string
+    Age  int
+    Tags map[string]string
+}
+
+func (u *User) String() string {
+    return fmt.Sprintf("Name:%s, Age:%d,Tags:%v", u.Name, u.Age, u.Tags)
+}
+
+type UserOptions interface {
+    Apply(user *User) error
+}
+
+type UserOption func(user *User) error
+
+func NewUserWithOptions(opts ...UserOption) *User {
+    user := &User{}
+    for _, opt := range opts {
+        err := opt(user)
+        if err != nil {
+            panic(err)
+        }
+    }
+    return user
+}
+
+func WithName(name string) UserOption {
+    return func(user *User) error {
+        user.Name = name
+        return nil
+    }
+}
+
+func WithAge(age int) UserOption {
+    return func(user *User) error {
+        user.Age = age
+        return nil
+    }
+}
+
+func WithTag(key, value string) UserOption {
+    return func(user *User) error {
+        if user.Tags == nil {
+            user.Tags = make(map[string]string)
+        }
+        user.Tags[key] = value
+        return nil
+    }
+}
+
+func main() {
+    user := NewUserWithOptions(WithName("Jacky"), WithAge(18), WithTag("key1", "value1"))
+    println(user.String())
 }
